9e9d3d57c1013936435719fe18e18d30abde1b94
Joseph Thomas <joseph@good-idea.studio>
2019-09-08 17:31:58 -0700

    chore(repo): set TS to strict

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,26 @@
 module.exports = {
-	// preset: 'ts-jest',
-	moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
-	setupFiles: ['./setupJest.ts'],
-	testMatch: ['**/**/*.test.ts', '**/**/*.test.tsx'],
-	coveragePathIgnorePatterns: ['node_modules', 'coverage', '/__.*__/', 'jest.config.js'],
-	testEnvironment: 'jsdom',
-	// collectCoverage: true,
-	collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
+  // preset: 'ts-jest',
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+  setupFiles: ['./setupJest.ts'],
+  testMatch: ['**/**/*.test.ts', '**/**/*.test.tsx'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'coverage',
+    '/__.*__/',
+    'jest.config.js',
+  ],
+  testEnvironment: 'jsdom',
+  // collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
 
-	transform: {
-		'^.+\\.tsx?$': 'ts-jest',
-	},
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      compilerOptions: {
+        strict: false,
+      },
+    },
+  },
 }
