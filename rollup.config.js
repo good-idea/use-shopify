5279d7bf7880cd59e86a99a5fde413938d8bc99a
Joseph Thomas <joseph@good-idea.studio>
2020-04-30 19:31:42 -0700

    fix(use-shopify): fix type exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import typescript from 'rollup-plugin-typescript2'
-
 import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
+  watch: {
+    include: 'src/**',
+  },
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
   ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
