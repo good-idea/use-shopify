f2d340d75cd5895cd0c6e509b711eeee246eb6c6
Joseph Thomas <joseph@good-idea.studio>
2020-04-30 20:56:45 -0700

    refactor(use-shopify): use generated types

--- src/types/lib.ts
+++ src/types/lib.ts
@@ -1,4 +1,6 @@
 import { DocumentNode } from 'graphql'
+import { Checkout, UserError } from './generated'
+
 /**
  * A function that implements uses a GraphQL library to imperitavely execute GraphQL queries and mutations.
  */
@@ -6,3 +8,10 @@ export type QueryFunction = <ExpectedResult, Variables = any>(
   query: string | DocumentNode,
   variables: Variables,
 ) => Promise<{ data: ExpectedResult }>
+
+export type CheckoutResponse<Key extends string> = {
+  [K in Key]: {
+    checkout?: Checkout
+    checkoutUserErrors?: UserError[]
+  }
+}
--- src/types/media.ts
+++
@@ -1,7 +0,0 @@
-export interface ShopifyImage {
-  id: string
-  originalSrc: string
-  altText?: string
-  transformedSrc?: string
-  __typename: 'Image'
-}
