764ee7727e2def8570d4ed6bd203e678c27c9c7d
Joseph Thomas <joseph@good-idea.studio>
2020-05-15 11:45:38 -0700

    fix(use-shopify): better type compatibility with partials/maybes

--- src/types/lib.ts
+++ src/types/lib.ts
@@ -1,6 +1,11 @@
 import { DocumentNode } from 'graphql'
 
 export type Maybe<T> = T | null
+
+export type DeepMaybe<T> = {
+  [K in keyof T]?: null | Maybe<T[K]>
+}
+
 /**
  * A function that implements uses a GraphQL library to imperitavely execute GraphQL queries and mutations.
  */
